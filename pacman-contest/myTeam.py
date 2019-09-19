03902cfa504cba9bb0f335f4a01d8ee306c9198e
Zongru Li <zongrul1@student.unimelb.edu.au>
2019-09-19 20:48:21 +1000

    simple_avoid and simple_greedy

--- pacman-contest/myTeam.py
+++ pacman-contest/myTeam.py
@@ -93,18 +93,7 @@ class DummyAgent(CaptureAgent):
     def changePhase(self):
         self.phase = 2
 
-    def getSuccessor(self, gameState, action):
-        """
-    Finds the next successor which is a grid position (location tuple).
-    """
-        successor = gameState.generateSuccessor(self.index, action)
-        pos = successor.getAgentState(self.index).getPosition()
-        if pos != nearestPoint(pos):
-            # Only half a grid position was covered
-            return successor.generateSuccessor(self.index, action)
-        else:
-            return successor
-
+        
     def getSuccessors(self, currentPosition):
         successors = []
         forbidden = self.walls
@@ -116,31 +105,6 @@ class DummyAgent(CaptureAgent):
                 nextPosition = (nx, ny)
                 successors.append((nextPosition, action))
         return successors
-
-    def evaluate(self, gameState, action):
-        """
-    Computes a linear combination of features and feature weights
-    """
-        features = self.getFeatures(gameState, action)
-        weights = self.getWeights(gameState, action)
-        return features * weights
-
-    def getFeatures(self, gameState, action):
-        """
-    Returns a counter of features for the state
-    """
-        features = util.Counter()
-        successor = self.getSuccessor(gameState, action)
-        features['successorScore'] = self.getScore(successor)
-        return features
-
-    def getWeights(self, gameState, action):
-        """
-    Normally, weights do not depend on the gamestate.  They can be either
-    a counter or a dictionary.
-    """
-        return {'successorScore': 1.0}
-
     # get middle
     def getMiddle(self, gameState):
         if self.red:
@@ -255,24 +219,6 @@ class OffensiveDummyAgent(DummyAgent):
             else:
                 return self.astarSearch(gameState, closeFood, self.simple_avoidEnemyHeurisitic)
 
-    def getFeatures(self, gameState, action):
-        features = util.Counter()
-        successor = self.getSuccessor(gameState, action)
-        foodList = self.getFood(successor).asList()
-        features['successorScore'] = -len(foodList)  # self.getScore(successor)
-
-        # Compute distance to the nearest food
-
-        if len(foodList) > 0:  # This should always be True,  but better safe than sorry
-            myPos = successor.getAgentState(self.index).getPosition()
-            minDistance = min([self.getMazeDistance(myPos, food) for food in foodList])
-            features['distanceToFood'] = minDistance
-        return features
-
-    def getWeights(self, gameState, action):
-        return {'successorScore': 100, 'distanceToFood': -1}
-
-
 class DefensiveDummyAgent(DummyAgent):
     """
   A reflex agent that keeps its side Pacman-free. Again,
@@ -337,3 +283,25 @@ class DefensiveDummyAgent(DummyAgent):
 
     def getWeights(self, gameState, action):
         return {'numInvaders': -1000, 'onDefense': 100, 'invaderDistance': -10, 'stop': -100, 'reverse': -2}
+
+    def evaluate(self, gameState, action):
+        """
+    Computes a linear combination of features and feature weights
+    """
+        features = self.getFeatures(gameState, action)
+        weights = self.getWeights(gameState, action)
+        return features * weights
+
+    def getSuccessor(self, gameState, action):
+        """
+    Finds the next successor which is a grid position (location tuple).
+    """
+        successor = gameState.generateSuccessor(self.index, action)
+        pos = successor.getAgentState(self.index).getPosition()
+        if pos != nearestPoint(pos):
+            # Only half a grid position was covered
+            return successor.generateSuccessor(self.index, action)
+        else:
+            return successor
+
+
